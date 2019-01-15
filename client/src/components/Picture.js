f24b700e161e5ff9dbc6a5124c6fb11221451e16
Isaac Kwon <isaac-kwon@hotmail.com>
2019-01-15 15:48:19 -0800

    remove Picture.js & refactor PictureDisplay.js

--- client/src/components/Picture.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Picture = () => (
-  <div style={{ border: '2px solid #ffffff', height: '25em', width: '25em', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-    <h1>Picture</h1>
-  </div>
-);
-
-export default Picture;
--- client/src/components/PictureDisplay.js
+++ client/src/components/PictureDisplay.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'antd';
-import Picture from './Picture';
 import SelectButton from './SelectButton';
 
-const PictureDisplay = () => {
-  return (
-    <div>
-      <Picture />
-      <div style={{ marginTop: '2em' }}>
-        <SelectButton title='Yee' type='primary' size='large' />
-        <SelectButton title='Nee' type='danger' size='large' />
+class PictureDisplay extends Component {
+  render() {
+    const styles = {
+      frame: {
+        border: '2px solid #ffffff',
+        height: '30em',
+        width: '25em',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      buttonGroup: {
+        marginTop: '2em'
+      }
+    }
+
+    return (
+      <div>
+        <div style={styles.frame}>
+          <h1>Picture</h1>
+        </div>
+        <div style={styles.buttonGroup}>
+          <SelectButton title='Yee' type='primary' size='large' />
+          <SelectButton title='Nee' type='danger' size='large' />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PictureDisplay;
