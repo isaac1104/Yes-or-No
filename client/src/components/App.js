5a1c2216962cf146479a1e1f74382c80b28aa226
Isaac Kwon <isaac-kwon@hotmail.com>
2019-01-15 11:54:24 -0800

    lazyload pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import requireAuth from './utils/requireAuth';
-import Landing from '../pages/Landing';
-import Home from '../pages/Home';
+
+const Landing = lazy(() => import('../pages/Landing'));
+const Home = lazy(() => import('../pages/Home'));
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/home' component={requireAuth(Home)} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/home' component={requireAuth(Home)} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     );
   }
