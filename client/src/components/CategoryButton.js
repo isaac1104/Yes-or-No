666510384b39f4a5822b64eddffaa7c0ec37b317
isaac1104 <isaac-kwon@hotmail.com>
2019-02-02 23:17:25 -0800

    [Gallery] add & restyle categories

--- client/src/components/CategoryButton.js
+++ client/src/components/CategoryButton.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Button } from 'antd';
 import { withRouter } from 'react-router-dom';
 
-const CategoryButton = ({ category, history }) => <Button onClick={() => history.push(`/gallery/${category}`)}>{category}</Button>;
+const CategoryButton = ({ category, history }) => (
+  <Button
+    onClick={() => history.push(`/gallery/${category}`)}
+    style={{ margin: '0.5em' }}
+    size='large'
+    type='primary'
+  >
+    {category}
+  </Button>
+);
 
 export default withRouter(CategoryButton);
--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -8,18 +8,34 @@ class Gallery extends Component {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '80vh'
+        flexWrap: 'wrap'
+      },
+      text: {
+        textAlign: 'center',
+        margin: '3em 0 3em 0'
       }
     };
 
     return (
-      <div style={styles.container} className='gallery'>
-        <CategoryButton category='Animals' />
-        <CategoryButton category='Architecture' />
-        <CategoryButton category='Fashion' />
-        <CategoryButton category='Food & Drink' />
-        <CategoryButton category='Nature' />
-        <CategoryButton category='Travel' />
+      <div className='gallery'>
+        <h3 style={styles.text}>Choose a Category</h3>
+        <div style={styles.container}>
+          <CategoryButton category='Animals' />
+          <CategoryButton category='Architecture' />
+          <CategoryButton category='Art & Culture' />
+          <CategoryButton category='Business & Work' />
+          <CategoryButton category='Current Events' />
+          <CategoryButton category='Experimental' />
+          <CategoryButton category='Fashion' />
+          <CategoryButton category='Food & Drink' />
+          <CategoryButton category='Health' />
+          <CategoryButton category='Nature' />
+          <CategoryButton category='People' />
+          <CategoryButton category='Spirituality' />
+          <CategoryButton category='Textures & Patterns' />
+          <CategoryButton category='Travel' />
+          <CategoryButton category='Wallpaper' />
+        </div>
       </div>
     )
   }
