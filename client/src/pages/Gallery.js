00d43b3c44c0d1e6f17f341242bbd0bca3652e09
isaac1104 <isaac-kwon@hotmail.com>
2019-02-02 23:22:21 -0800

    [Gallery] refactor rendering category buttons

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import CategoryButton from '../components/CategoryButton';
 
 class Gallery extends Component {
+  renderCategories() {
+    const categories = [
+      'Animals',
+      'Architecture',
+      'Art & Culture',
+      'Business & Work',
+      'Current Event',
+      'Experimental',
+      'Fashion',
+      'Food & Drink',
+      'Health',
+      'Nature',
+      'People',
+      'Spirituality',
+      'Textures & Patterns',
+      'Travel',
+      'Wallpaper'
+    ];
+
+    return categories.map(category => {
+      return <CategoryButton category={category} key={category} />;
+    });
+  }
+
   render() {
     const styles = {
       container: {
@@ -20,21 +44,7 @@ class Gallery extends Component {
       <div className='gallery'>
         <h3 style={styles.text}>Choose a Category</h3>
         <div style={styles.container}>
-          <CategoryButton category='Animals' />
-          <CategoryButton category='Architecture' />
-          <CategoryButton category='Art & Culture' />
-          <CategoryButton category='Business & Work' />
-          <CategoryButton category='Current Events' />
-          <CategoryButton category='Experimental' />
-          <CategoryButton category='Fashion' />
-          <CategoryButton category='Food & Drink' />
-          <CategoryButton category='Health' />
-          <CategoryButton category='Nature' />
-          <CategoryButton category='People' />
-          <CategoryButton category='Spirituality' />
-          <CategoryButton category='Textures & Patterns' />
-          <CategoryButton category='Travel' />
-          <CategoryButton category='Wallpaper' />
+          {this.renderCategories()}
         </div>
       </div>
     )
